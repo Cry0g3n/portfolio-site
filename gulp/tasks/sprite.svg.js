0c92624987fb720a25e9c64d958d6428a19e6ee2
Andrew <andrey.shemiakov@gmail.com>
2017-02-16 00:24:27 +0300

    Доработки таска sprite:svg

--- gulp/tasks/sprite.svg.js
+++ gulp/tasks/sprite.svg.js
@@ -20,10 +20,10 @@ module.exports = function () {
             .pipe($.gp.svgSprite({
                 mode: {
                     symbol: {
-                        sprite: "../sprite.svg"
-                    },
-                    example: {
-                        dest: '../tmp/svgDemo.html'
+                        sprite: "../sprite.svg",
+                        example: {
+                            dest: '../tmp/svgDemo.html'
+                        }
                     }
                 }
             }))
